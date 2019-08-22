f6dfb4bd755a45fa7b29dac3d55dc03f4b8c245b
David Hardy <davehardy632@gmail.com>
2019-08-22 17:35:36 -0600

    Creating a database table for a recipe with fields of most useful information.

---
+++ migrations/20190822232143-create-recipe.js
@@ -0,0 +1,66 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Recipes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      foodType: {
+        type: Sequelize.STRING
+      },
+      thumbnailImg: {
+        type: Sequelize.STRING
+      },
+      ingredientNum: {
+        type: Sequelize.INTEGER
+      },
+      prepTime: {
+        type: Sequelize.FLOAT
+      },
+      ingredients: {
+        type: Sequelize.TEXT
+      },
+      calories: {
+        type: Sequelize.FLOAT
+      },
+      url: {
+        type: Sequelize.STRING
+      },
+      dietLabel: {
+        type: Sequelize.STRING
+      },
+      healthLabel: {
+        type: Sequelize.STRING
+      },
+      cautions: {
+        type: Sequelize.STRING
+      },
+      fat: {
+        type: Sequelize.FLOAT
+      },
+      carbs: {
+        type: Sequelize.FLOAT
+      },
+      protein: {
+        type: Sequelize.FLOAT
+      },
+      yield: {
+        type: Sequelize.FLOAT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Recipes');
+  }
+};
