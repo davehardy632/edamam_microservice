0ebe9aa684676a7f328aa7999b5c106d37b733e7
davisc0801 <davisc1014@gmail.com>
2019-08-24 18:19:45 -0600

    Add test for searching recipes by calorie count

---
+++ spec/api/v1/calorie_search.spec.js
@@ -0,0 +1,38 @@
+var request = require("supertest")
+var app = require("../../../app")
+const express = require("express");
+var router = express.Router();
+
+describe('api', () => {
+  describe ("api v1 calorie search path", () => {
+    test("should return a 202 status and all recipes", () => {
+      recipeValues =  [ 'id', 'foodType', 'recipeName', 'thumbnailImg', 'ingredientNum', 'prepTime', 'ingredients', 'calories', 'url', 'dietLabel', 'healthLabel', 'cautions', 'fat', 'carbs', 'protein', 'yield' ]
+      return request(app).get("/api/v1/recipes/calorie_search?q=1000")
+      .set('Accept', 'application/json')
+      .then(response => {
+        expect(response.statusCode).toBe(202);
+        expect(Object.keys(response.body).length).toEqual(3);
+        expect(Object.keys(response.body[0])).toEqual(recipeValues);
+        expect(Object.keys(response.body[2])).toEqual(recipeValues);
+      });
+    });
+
+    test("should return a 404 status and an error for missing parameter", () => {
+      return request(app).get("/api/v1/recipes/calorie_search")
+      .set('Accept', 'application/json')
+      .then(response => {
+        expect(response.statusCode).toBe(404);
+        expect(response.body).toEqual({"error": "Missing query parameter."});
+      });
+    });
+
+    test("should return a 404 status and error message for no matching recipes", () => {
+      return request(app).get("/api/v1/recipes/calorie_search?q=walnuts")
+      .set('Accept', 'application/json')
+      .then(response => {
+        expect(response.statusCode).toBe(406);
+        expect(response.body).toEqual({"error": "Invalid input."});
+      })
+    });
+  });
+});
