98f99275dfde77f743b23160639433e8e32a263e
davisc0801 <davisc1014@gmail.com>
2019-08-24 20:50:11 -0600

    Add route to search for recipes within a calorie target

--- routes/api/v1/recipes.js
+++ routes/api/v1/recipes.js
@@ -1,10 +1,11 @@
 var express = require("express");
+var sequelize = require("sequelize");
 var router = express.Router();
 var Recipe = require('../../../models').Recipe;
+const recipeAttributes = ["id", "foodType", "recipeName", "thumbnailImg", "ingredientNum", "prepTime", "ingredients", "calories", "url", "dietLabel", "healthLabel", "cautions", "fat", "carbs", "protein", "yield"]
 
 router.get("/food_search", async function(req, res, next) {
   foodType = req.url.split("=")[1]
-  recipeAttributes = ["id", "foodType", "recipeName", "thumbnailImg", "ingredientNum", "prepTime", "ingredients", "calories", "url", "dietLabel", "healthLabel", "cautions", "fat", "carbs", "protein", "yield"]
   if (!foodType) {
     noArgumentError = {"message": "Missing query parameter"}
     res.setHeader("Content-Type", "application/json");
@@ -22,10 +23,20 @@ router.get("/food_search", async function(req, res, next) {
   }
 });
 
+router.get("/calorie_search", async function(req, res) {
+  let calorieQuery = parseInt(req.query.q);
+  if (calorieQuery) {
+      recipes = await recipesByCalorieAmount(calorieQuery, recipeAttributes, 3)
+      res.setHeader("Content-Type", "application/json");
+      res.status(202).send(JSON.stringify(recipes));
+  } else {
+    res.setHeader("Content-Type", "application/json");
+    res.status(406).send(JSON.stringify({error: "Invalid input."}));
+}});
+
 
 router.get("/servings_search", async function(req, res, next) {
   servingSize = parseInt(req.url.split("=")[1])
-  recipeAttributes = ["id", "foodType", "recipeName", "thumbnailImg", "ingredientNum", "prepTime", "ingredients", "calories", "url", "dietLabel", "healthLabel", "cautions", "fat", "carbs", "protein", "yield"]
   if (!servingSize) {
     noArgumentError = {"message": "Missing query parameter"}
     res.setHeader("Content-Type", "application/json");
@@ -49,6 +60,26 @@ let recipesByFoodType = async (food, attributes) => {
   let recipes = await Recipe.findAll({ where: {foodType: food}, attributes: attributes })
     .then(recipes => {
       return recipes;
+    })
+    .catch(error => {
+      return error
+    });
+  return recipes;
+}
+
+let recipesByCalorieAmount = async (calorieQuery, attributes, limit) => {
+  let recipes = await Recipe.findAll({
+      attributes: attributes,
+      order: sequelize.col('calories')
+    })
+    .then(recipes => {
+      let recipeStack = [];
+      recipes.forEach(recipe => {
+        recipeStack.push({recipe: recipe, difference: Math.abs(recipe.calories - calorieQuery)})
+      });
+      recipeStack.sort((a, b) => a.difference - b.difference);
+      let finalArray = recipeStack.slice(0,limit);
+      return finalArray.map(element => element["recipe"]["dataValues"]);
     })
     .catch(error => {
       return error
