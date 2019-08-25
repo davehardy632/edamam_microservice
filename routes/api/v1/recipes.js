00251916f068a30e7daee61d40a6b98399dd1e17
David Hardy <davehardy632@gmail.com>
2019-08-24 18:07:23 -0600

    Finishing the endpoint to return recipes based on serving size, also accounting for edge cases.

--- routes/api/v1/recipes.js
+++ routes/api/v1/recipes.js
@@ -22,10 +22,42 @@ router.get("/food_search", async function(req, res, next) {
   }
 });
 
+
+router.get("/servings_search", async function(req, res, next) {
+  servingSize = parseInt(req.url.split("=")[1])
+  recipeAttributes = ["id", "foodType", "recipeName", "thumbnailImg", "ingredientNum", "prepTime", "ingredients", "calories", "url", "dietLabel", "healthLabel", "cautions", "fat", "carbs", "protein", "yield"]
+  if (!servingSize) {
+    noArgumentError = {"message": "Missing query parameter"}
+    res.setHeader("Content-Type", "application/json");
+    res.status(404).send(JSON.stringify(noArgumentError));
+  } else {
+    recipes = await recipesByServingSize(servingSize, recipeAttributes)
+    if (recipes.length != 0) {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).send(JSON.stringify(recipes));
+    } else {
+      invalidServingError = {"message": `There are no recipes with ${servingSize} servings`}
+      res.setHeader("Content-Type", "application/json");
+      res.status(404).send(JSON.stringify(invalidServingError));
+    }
+  }
+});
+
 //////// async helper functions\
 
 let recipesByFoodType = async (food, attributes) => {
   let recipes = await Recipe.findAll({ where: {foodType: food}, attributes: attributes })
+    .then(recipes => {
+      return recipes;
+    })
+    .catch(error => {
+      return error
+    });
+  return recipes;
+}
+
+let recipesByServingSize = async (servings, attributes) => {
+  let recipes = await Recipe.findAll({ where: {yield: servings}, attributes: attributes })
     .then(recipes => {
       return recipes;
     })
